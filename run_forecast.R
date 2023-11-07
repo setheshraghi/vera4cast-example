cbcd82729631e39d7e5e3fe230b15e7b08c55783
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-11-07 15:13:03 -0500

    example with multiple sites

--- run_forecast.R
+++ run_forecast.R
@@ -10,21 +10,28 @@ source('R/get_weather.R')
 forecast_date <- Sys.Date()
 site_list <- read_csv("https://raw.githubusercontent.com/LTREB-reservoirs/vera4cast/main/vera4cast_field_site_metadata.csv",
                       show_col_types = FALSE)
-
+model_id <- 'TempC_mean_example_forecast'
 # this should generate a df
-example_forecast_file <- generate_example_forecast(forecast_date = forecast_date,
-                                                   model_id = 'TempC_mean_example_forecast',
+forecast <- generate_example_forecast(forecast_date = forecast_date,
+                                                   model_id = model_id,
                                                    targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
                                                    var = 'Temp_C_mean',
                                                    site = 'fcre')
 
 # example for multiple sites
 site <- c('fcre', 'bvre')
-example_forecast_file <- site |>
-  map_dfr(generate_example_forecast, forecast_date = forecast_date,
-                                                   model_id = 'TempC_mean_example_forecast',
-                                                   targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
-                                                   var = 'Temp_C_mean')
+forecast <- site |>
+  map(generate_example_forecast,
+      forecast_date = forecast_date,
+      model_id = model_id,
+      targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
+      var = 'Temp_C_mean') |>
+  list_rbind()
+
+# write forecast locally
+forecast_file <- paste0('Forecasts/', forecast_date, '-', model_id, '.csv')
+
+write_csv(forecast, forecast_file)
 
 # Submit forecast!
-vera4castHelpers::submit(forecast_file = example_forecast_file)
+vera4castHelpers::submit(forecast_file = forecast_file)
