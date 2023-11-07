c41bd4992a5df5460d1259f1db0bb664363927d9
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-11-07 15:14:23 -0500

    formatting

--- run_forecast.R
+++ run_forecast.R
@@ -5,28 +5,20 @@ source('load_packages.R')
 source('R/generate_example_forecast.R')
 source('R/get_weather.R')
 
-# Generate the forecasts
+# ---- Generate the forecasts -----
 # default is to run a real-time forecast for today
 forecast_date <- Sys.Date()
 site_list <- read_csv("https://raw.githubusercontent.com/LTREB-reservoirs/vera4cast/main/vera4cast_field_site_metadata.csv",
                       show_col_types = FALSE)
 model_id <- 'TempC_mean_example_forecast'
+
 # this should generate a df
 forecast <- generate_example_forecast(forecast_date = forecast_date,
                                                    model_id = model_id,
                                                    targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
                                                    var = 'Temp_C_mean',
                                                    site = 'fcre')
-
-# example for multiple sites
-site <- c('fcre', 'bvre')
-forecast <- site |>
-  map(generate_example_forecast,
-      forecast_date = forecast_date,
-      model_id = model_id,
-      targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
-      var = 'Temp_C_mean') |>
-  list_rbind()
+#----------------------------------------#
 
 # write forecast locally
 forecast_file <- paste0('Forecasts/', forecast_date, '-', model_id, '.csv')
@@ -35,3 +27,14 @@ write_csv(forecast, forecast_file)
 
 # Submit forecast!
 vera4castHelpers::submit(forecast_file = forecast_file)
+
+#-------------------------------------------#
+# example for multiple sites
+# site <- c('fcre', 'bvre')
+# forecast <- site |>
+#   map(generate_example_forecast,
+#       forecast_date = forecast_date,
+#       model_id = model_id,
+#       targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
+#       var = 'Temp_C_mean') |>
+#   list_rbind()
