2bf6b321970d3736471c11b79bf7abc9b1928559
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-11-07 14:34:39 -0500

    get daily or hourly weather

--- R/get_weather.R
+++ R/get_weather.R
@@ -1,4 +1,4 @@
-get_weather <- function(current_site, site_list) {
+get_hourly_weather <- function(current_site, site_list, past = 90, future = 30) {
 
   lat <- site_list |>
     filter(site_id == current_site) |>
@@ -12,12 +12,40 @@ get_weather <- function(current_site, site_list) {
   site_weather <- RopenMeteo::get_ensemble_forecast(
     latitude = lat,
     longitude = long,
-    forecast_days = 30, # days into the future
-    past_days = 60, # past days that can be used for model fitting
+    forecast_days = future, # days into the future
+    past_days = past, # past days that can be used for model fitting
     model = "gfs_seamless",
     variables = c("temperature_2m")) |>
     RopenMeteo::convert_to_efi_standard() |>
     mutate(site_id = current_site)
+
+  return(site_weather)
+}
+
+
+get_daily_weather <- function(current_site, site_list, past = 90, future = 30) {
+
+  lat <- site_list |>
+    filter(site_id == current_site) |>
+    select(latitude) |>  pull()
+
+  long <-  site_list |>
+    filter(site_id == current_site) |>
+    select(longtitude) |>  pull()
+
+
+  site_weather <- RopenMeteo::get_ensemble_forecast(
+    latitude = lat,
+    longitude = long,
+    forecast_days = future, # days into the future
+    past_days = past, # past days that can be used for model fitting
+    model = "gfs_seamless",
+    variables = c("temperature_2m")) |>
+    RopenMeteo::convert_to_efi_standard() |>
+    mutate(site_id = current_site,
+           datetime = as_date(datetime)) |>
+    group_by(datetime, site_id, variable, parameter) |>
+    summarise(prediction = mean(prediction))
 
   return(site_weather)
 }
