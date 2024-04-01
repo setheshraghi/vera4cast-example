69704d7b7ff62d171e61db4681d344a63a65162b
Freya Olsson <freyao@vt.edu>
2024-04-01 18:21:44 +0000

    add vars to get_weather

--- R/generate_example_forecast.R
+++ R/generate_example_forecast.R
@@ -1,9 +1,9 @@
 # generate_example_forecast
-generate_example_forecast <- function(forecast_date,
+generate_example_forecast <- function(forecast_date, # a recommended argument so you can pass the date to the function
                                       model_id,
-                                      targets_url,
-                                      var,
-                                      site,
+                                      targets_url, # where are the targets you are forecasting?
+                                      var, # what variable(s)?
+                                      site, # what site(s)
                                       forecast_depths = 'focal',
                                       project_id = 'vera4cast') {
 
@@ -18,6 +18,8 @@ generate_example_forecast <- function(forecast_date,
   if (site == 'bvre' & forecast_depths == 'focal') {
     forecast_depths <- 1.5
   }
+  #-------------------------------------
+
   # Get targets
   message('Getting targets')
   targets <- readr::read_csv(targets_url, show_col_types = F) |>
@@ -25,6 +27,7 @@ generate_example_forecast <- function(forecast_date,
            site_id %in% site,
            depth_m %in% forecast_depths,
            datetime < forecast_date)
+  #-------------------------------------
 
   # Get the weather data
   message('Getting weather')
@@ -33,10 +36,10 @@ generate_example_forecast <- function(forecast_date,
     # you can modify the data that are collected in the get_daily_weather function
     # or if you want to generate an hourly forecast, you can use get_hourly_weather
   weather_dat <- site |>
-    map_dfr(get_daily_weather, site_list = site_list, past = 60, future = 30)
+    map_dfr(get_daily_weather, site_list = site_list, past = 60, future = 30, vars = "temperature_2m")
+  #-------------------------------------
 
   # split it into historic and future
-
    historic_weather <- weather_dat |>
     filter(datetime < forecast_date) |>
     # calculate a daily mean (remove ensemble)
@@ -45,12 +48,11 @@ generate_example_forecast <- function(forecast_date,
     pivot_wider(names_from = variable, values_from = prediction) |>
     mutate(air_temperature = air_temperature - 273.15)
 
-
   forecast_weather <- weather_dat |>
     filter(datetime >= forecast_date) |>
     pivot_wider(names_from = variable, values_from = prediction) |>
     mutate(air_temperature = air_temperature - 273.15)
-
+  #-------------------------------------
 
   # Fit model
   message('Fitting model')
@@ -59,6 +61,7 @@ generate_example_forecast <- function(forecast_date,
     left_join(historic_weather)
 
   model_fit <- lm(fit_df$Temp_C_mean ~ fit_df$air_temperature)
+  #-------------------------------------
 
   # Generate forecasts
   message('Generating forecast')
@@ -75,6 +78,7 @@ generate_example_forecast <- function(forecast_date,
                             depth_m = forecast_depths,
                             duration = targets$duration[1],
                             project_id = project_id)
+  #-------------------------------------
 
   return(forecast_df)
 
