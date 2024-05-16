f8254f1004b970db03d5df1b3b030c07a535c950
Freya Olsson <freyao@vt.edu>
2024-05-16 18:27:33 +0000

    simplify run forecast

--- forecast_code/run_forecast.R
+++ forecast_code/run_forecast.R
@@ -4,14 +4,22 @@ setwd(here::here())
 source('forecast_code/load_packages.R')
 
 # load the forecast generation function - include at least a forecast_date argument
-source('R/generate_example_forecast.R')
-source('R/get_weather.R') # wrapper around the RopenMeteo package to get weather covariates
+source('forecast_code/R/generate_example_forecast.R')
 
 # ---- Generate the forecasts -----
-# default is to run a real-time forecast for today
+# default is to run a real-time forecast for today for fcre only
 forecast_date <- Sys.Date()
 site_list <- read_csv("https://raw.githubusercontent.com/LTREB-reservoirs/vera4cast/main/vera4cast_field_site_metadata.csv",
                       show_col_types = FALSE)
+
+fcre_lat <- site_list |>
+  filter(site_id == 'fcre') |>
+  pull(latitude)
+
+fcre_long <- site_list |>
+  filter(site_id == 'fcre') |>
+  pull(longitude)
+
 model_id <- 'TempC_mean_example_forecast'
 
 # this should generate a df
@@ -20,10 +28,13 @@ forecast <- generate_example_forecast(forecast_date = forecast_date,
                                       targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
                                       var = 'Temp_C_mean',
                                       site = 'fcre',
+                                      lat = fcre_lat,
+                                      long = fcre_long,
                                       forecast_depths = 'focal')
 #----------------------------------------#
 
 # write forecast locally
+message('Writing forecast')
 save_here <- 'Forecasts/'
 forecast_file <- paste0(save_here, forecast_date, '-', model_id, '.csv')
 
@@ -34,12 +45,14 @@ if (dir.exists(save_here)) {
   write_csv(forecast, forecast_file)
 }
 
-
-
 # Submit forecast!
 vera4castHelpers::submit(forecast_file = forecast_file)
 
 #-------------------------------------------#
+
+# Here is an example that use the purrr package to run the above function over multiple of sites.
+# The map functions (map, map2, pmap) can be useful for this type of iteration
+
 # example for multiple sites
 # site <- c('fcre', 'bvre')
 # forecast <- site |>
@@ -47,5 +60,8 @@ vera4castHelpers::submit(forecast_file = forecast_file)
 #       forecast_date = forecast_date,
 #       model_id = model_id,
 #       targets_url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz",
-#       var = 'Temp_C_mean') |>
-#   list_rbind()
+#       var = 'Temp_C_mean',
+#       lat = fcre_lat,
+#       long = fcre_long,
+#       forecast_depths = 'focal') |>
+#   list_rbind() # combines the output from the multiple map runs (a list of dfs)
